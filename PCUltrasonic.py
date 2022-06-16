56fdfe675aadf24036d9d91e6ddc0c298778314f
David Calles <yodavidcalles@gmail.com>
2022-06-15 23:12:35 -0400

    Working (graph not tested) MQTT receiver for ultrasonic data

--- PCUltrasonic.py
+++ PCUltrasonic.py
@@ -10,13 +10,36 @@ import sys
 sys.path.append('Network_Connections/MQTT_Connection/')
 import MQTT_Receiver as mymqttr
 import pandas as pd
+import numpy as np
+import plotly.express as px
 ##
    
 ## Block terminal for 'segs' seconds and receive data 
-connection0 = mymqttr.NewMQTTReceiver("pc_lin0", segs=20)
+connection0 = mymqttr.NewMQTTReceiver("pc_lin0", segs=11)
+connection0.disconnect()
 myData = mymqttr.data
-print(myData)
 
+# Turn data to a dataframe
 dfColumns = ['SampleId', 'DateTime(UTC)', 'RawDistance(cm)', 'CalibratedDistance(cm)']
 dfUltrasonic = pd.DataFrame(data=myData, columns=dfColumns)
-print(dfUltrasonic.head())
+dfUltrasonic = dfUltrasonic.astype({'SampleId': 'int32', 'RawDistance(cm)':'datetime64',
+                                    'RawDistance(cm)': 'float64', 'CalibratedDistance(cm)': 'float'})
+#print(dfUltrasonic.info())
+print("\n", dfUltrasonic.head())
+
+# Analytics 
+print("\n/*----------------------- ANALYTICS --------------------------*/\n")
+print(f"Received samples: {len(dfUltrasonic['SampleId'])}")
+missingSamples = np.count_nonzero(np.absolute(np.diff(dfUltrasonic['SampleId'].to_numpy())) > 1)
+print(f"Lost samples: {missingSamples}")
+print("Statistics:\n")
+print(dfUltrasonic.describe())
+# Write data to json
+jsonPath = "UltrasonicSensor/RobotUltraSensor/testData.json"
+dfUltrasonic.to_json(jsonPath, orient='records', indent=2)
+
+# Plot
+fig = px.line(dfUltrasonic, x="DateTime(UTC)",
+              y=["RawDistance(cm)", "CalibratedDistance(cm)"],
+              title='Ultrasensor Data', template='plotly_dark')
+fig.show() 
