cceccae1e47cc0df46b166d91a4aae0b7be3fead
David Calles <yodavidcalles@gmail.com>
2022-06-16 16:32:49 -0400

    Cleansed comments

--- PCReceiveMQTTUltrasonic.py
+++ PCReceiveMQTTUltrasonic.py
@@ -1,11 +1,6 @@
 # importing sys
 import sys
-# # importing picar-x classes to the system path
-# sys.path.append('RobotSource/picar-x/')
-# import picarx as pix
-# # importing own classes for ultrasonic sensor calibration
-# sys.path.append('UltrasonicSensor/RpiUltrasonic/')
-# import RpiUltrasonic as rpius
+
 # importing own functions for mqtt connetion
 sys.path.append('Network_Connections/MQTT_Connection/')
 import MQTT_Receiver as mymqttr
@@ -15,7 +10,7 @@ import plotly.express as px
 ##
    
 ## Block terminal for 'segs' seconds and receive data 
-connection0 = mymqttr.NewMQTTReceiver("pc_lin0", segs=11)
+connection0 = mymqttr.NewMQTTReceiver("pc_lin0", segs=10)
 connection0.disconnect()
 myData = mymqttr.data
 
@@ -42,4 +37,4 @@ dfUltrasonic.to_json(jsonPath, orient='records', indent=2)
 fig = px.line(dfUltrasonic, x="DateTime(UTC)",
               y=["RawDistance(cm)", "CalibratedDistance(cm)"],
               title='Ultrasensor Data', template='plotly_dark')
-fig.show() 
+fig.show()
