3a3c3f5664e38bdb0359604d065a7b754d1e988e
David Calles <yodavidcalles@gmail.com>
2022-07-12 11:33:05 -0400

    Added dependencies for SLAM and Test running tasks on background

---
+++ Background_Tasks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run User-Input related code
+
+python3 UserInput/TestOutputToFile.py > UserInput/OutputText.txt &
---
+++ UserInput/TestOutputToFile.py
@@ -0,0 +1,7 @@
+i=0
+import time
+
+for x in range(20):
+    print(f"Hello {i}\n")
+    i+=1
+    time.sleep(1)
