0c699ff835d2cb27b847c27bfd6f94c1aad2739a
Govinda Bhatt <105728231+peakymonk@users.noreply.github.com>
2022-07-12 10:46:34 -0400

    Update RobotTests.py
    added function to test each servo motors

--- RobotTests.py
+++ RobotTests.py
@@ -40,6 +40,11 @@ speed = 10
 currentDirAngle = 0
 angleLeftIncrement = 5
 angleRightIncrement = -5
+angle1upIncrement = 5
+angle1downIncrement = -5
+angle2upIncrement = 5
+angle2downIncrement = 5
+
 
 while True:
 
@@ -60,6 +65,42 @@ while True:
         elif keyboard.is_pressed('d'):
             currentDirAngle += angleRightIncrement
             myrobot.set_dir_servo_angle(currentDirAngle)
+         
+        elif keyboard.is_pressed('1'):
+            myrobot.self.set_motor_speed(1, speed)
+            
+        elif keyboard.is_pressed('2'):
+            myrobot.self.set_motor_speed(2, speed)
+            
+        elif keyboard.is_pressed('3'):
+            currentDirAngle += angle1upIncrement
+            myrobot.self.camera_servo_pin1.angle(currentDirAngle)
+            
+        elif keyboard.is_pressed('4'):
+            currentDirAngle += angle1downIncrement
+            myrobot.self.camera_servo_pin1.angle(currentDirAngle)
+            
+        elif keyboard.is_pressed('5'):
+            currentDirAngle += angle2upIncrement
+            myrobot.self.camera_servo_pin2.angle(currentDirAngle)
+            
+        elif keyboard.is_pressed('6'):
+            currentDirAngle += angle2downIncrement
+            myrobot.self.camera_servo_pin2.angle(currentDirAngle)
+            
+        elif keyboard.is_pressed('7'):
+            currentDirAngle += angleLeftIncrement
+            myrobot.self.dir_servo_pin.angle(currentDirAngle)
+            
+        elif keyboard.is_pressed('8'):
+            currentDirAngle += angleRingtIncrement
+            myrobot.self.dir_servo_pin.angle(currentDirAngle)
+            
+        elif keyboard.is_pressed('0'):
+            currentDirAngle += angleLeftIncrement
+            myrobot.self.dir_servo_pin.angle(0)
+            myrobot.self.camera_servo_pin1.angle(0)
+            myrobot.self.camera_servo_pin2.angle(currentDirAngle)
         
         time.sleep(motorDelay)
         myrobot.stop()
