075691f40208069e234867197e499b5180915590
David Calles <yodavidcalles@gmail.com>
2022-07-19 10:25:52 -0400

    Changed destination directories to be absolute in getWifiData.sh

--- Retrieve_Wi-Fi_Data/getWiFiData.sh
+++ Retrieve_Wi-Fi_Data/getWiFiData.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 
+PROJECT_DIR='/home/davidcalles/Documents/Wi-Fi-Analizer-Robot/'
+OUTPUT_DIR='Retrieve_Wi-Fi_Data/'
+
 #get parameters
-iwlist wlan0 scan | grep 'ESSID\|Quality\|Channel\|Frequency\|Encryption' > data
+iwlist wlan0 scan | grep 'ESSID\|Quality\|Channel\|Frequency\|Encryption' > "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 
 #split quality and signal level
-sed -i 's/Signal level/\nSignal level/g' data
+sed -i 's/Signal level/\nSignal level/g' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 
 #get rid of all characters but numbers and ESSID
 
 #replace "=" with ":"
-sed -i 's/=/:/g' data
+sed -i 's/=/:/g' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #replace """ with ""
-sed -i 's/"//g' data
+sed -i 's/"//g' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #replace "/" with ","
-sed -i 's/\//,/g' data
+sed -i 's/\//,/g' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #deletes everything after GHz 
-sed -i 's/.GHz*//' data
+sed -i 's/.GHz*//' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #deletes everything in ()
-sed -i -e 's/([^()]*)//g' data
+sed -i -e 's/([^()]*)//g' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #deletes everything after dbm 
-sed -i 's/.dBm*//' data
+sed -i 's/.dBm*//' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 
 #remove leading spaces
-sed -i -e 's/^[ \t]*//' data
+sed -i -e 's/^[ \t]*//' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #adds --- at the end of each individual network
-sed -i 's/^ESSID.*/&---/' data
+sed -i 's/^ESSID.*/&---/' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #deletes everything before the  : 
-sed -i 's/^.*://' data
+sed -i 's/^.*://' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #remove trailng spaces
-sed -i 's/[ \t]*$//' data
+sed -i 's/[ \t]*$//' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 
 
 #add ","
-sed -i 's/$/,/g' data
+sed -i 's/$/,/g' "${PROJECT_DIR}${OUTPUT_DIR}data.txt"
 #deletes \n
-tr '\n' ' ' < data > data.csv
+tr '\n' ' ' < "${PROJECT_DIR}${OUTPUT_DIR}data.txt" > "${PROJECT_DIR}${OUTPUT_DIR}data.csv"
 #removes spaces after ,
-sed -i 's/, /,/g' data.csv
+sed -i 's/, /,/g' "${PROJECT_DIR}${OUTPUT_DIR}data.csv"
 
 #adds a new line at the begining of each network
-sed -i 's/---,/\n/g' data.csv
+sed -i 's/---,/\n/g' "${PROJECT_DIR}${OUTPUT_DIR}data.csv"
 #add new line at the begining
-sed -i '1s/^/\n/' data.csv
+sed -i '1s/^/\n/' "${PROJECT_DIR}${OUTPUT_DIR}data.csv"
 
 #adds heather
-echo "$(echo -n 'Channel,Frequency,Quality,Quality Max,Signal Level,Encryption,SSID'; cat data.csv)" > data.csv
+echo "$(echo -n 'Channel,Frequency,Quality,Quality Max,Signal Level,Encryption,SSID'; cat ${PROJECT_DIR}${OUTPUT_DIR}data.csv)" > "${PROJECT_DIR}${OUTPUT_DIR}data.csv"
 
 #deletes last line
 #sed -i '$d' data.csv
 
 #genrate json
-cat data.csv| python3 -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > wifi.json
+cat "${PROJECT_DIR}${OUTPUT_DIR}data.csv" | python3 -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' > "${PROJECT_DIR}${OUTPUT_DIR}wifi.json"
