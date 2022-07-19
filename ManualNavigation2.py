33a84c31fd8ad8ad37edd9610e9f7f6560d12178
David Calles <yodavidcalles@gmail.com>
2022-07-18 22:20:43 -0400

    Tested manual navigation
    - All motors tested and working
    - Ultrasonic sensor guard to avoid frontal crash

--- ManualNavigation2.py
+++ ManualNavigation2.py
@@ -10,34 +10,69 @@ from picarx import *
 #-------------------------- Robot Parameters -------------------------#
 myrobot = Picarx()
 speed = 50
-steerIncrement = 5
-initialAngle = 0
+dirAngle_Increment = 8
+dirAngle_Start = 0
+xCamera_Increment = 2
+xCamera_Start = 0
+yCamera_Increment = 2
+yCamera_Start = 0
+ultraSensorGuardDistance = 6 #cm
 
 #-------------------------- Check for stdin terminal -------------------------#
 def isData():
     return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
-#-------------------------- Movement Funtions -------------------------#
+#-------------------------- Movement Funtions --------------------------------#
+#--------------------------------------------- Steering ----------------------#
 def robot_SoftSteerRight():
-    global initialAngle
-    initialAngle += steerIncrement
-    myrobot.set_dir_servo_angle(initialAngle)
+    global dirAngle_Start
+    dirAngle_Start += dirAngle_Increment
+    myrobot.set_dir_servo_angle(dirAngle_Start)
     
 def robot_SoftSteerLeft():
-    global initialAngle
-    initialAngle -= steerIncrement
-    myrobot.set_dir_servo_angle(initialAngle)
-    
+    global dirAngle_Start
+    dirAngle_Start -= dirAngle_Increment
+    myrobot.set_dir_servo_angle(dirAngle_Start) 
+
+#------------------------------------------- Forward/Backward ----------------#    
 def robot_SoftForward():
-    myrobot.forward(speed)
+    if(myrobot.get_distance() > 6):
+        myrobot.forward(speed)
     
 def robot_SoftBackward():
     myrobot.backward(speed)
 
-commands = [['w', robot_SoftForward],
+#------------------------------------------ Camera Horizontal ----------------#
+def robot_SoftLookLeft():
+    global xCamera_Start
+    xCamera_Start -= xCamera_Increment
+    myrobot.set_camera_servo1_angle(xCamera_Start)
+    
+def robot_SoftLookRight():
+    global xCamera_Start
+    xCamera_Start += xCamera_Increment
+    myrobot.set_camera_servo1_angle(xCamera_Start)
+
+#------------------------------------------ Camera Vertical ------------------#
+def robot_SoftLookUp():
+    global yCamera_Start
+    yCamera_Start += yCamera_Increment
+    myrobot.set_camera_servo2_angle(yCamera_Start)
+    
+def robot_SoftSteerDown():
+    global yCamera_Start
+    yCamera_Start -= yCamera_Increment
+    myrobot.set_camera_servo2_angle(yCamera_Start)
+
+commands = [['w', robot_SoftForward],   
             ['a', robot_SoftSteerLeft],
             ['s', robot_SoftBackward],
-            ['d', robot_SoftSteerRight]]
+            ['d', robot_SoftSteerRight],
+
+            ['q', robot_SoftLookLeft],
+            ['e', robot_SoftLookRight],
+            ['r', robot_SoftLookUp],
+            ['f', robot_SoftSteerDown]]
             
 if __name__ == '__main__':
 
@@ -52,7 +87,7 @@ if __name__ == '__main__':
                 c = sys.stdin.read(1)
                 
                 # SAMPLE COMPARISON
-                if c == 'f':
+                if c == 't':
                     print("OMG lol4")
                       
                 # BASIC MANUAL CONTROL
