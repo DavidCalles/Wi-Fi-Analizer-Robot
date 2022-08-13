f8f7e6075dca6fd9857c78b9b0dce6950b31d1ea
David Calles <yodavidcalles@gmail.com>
2022-08-13 13:24:34 -0400

    Cleaning repo from unused or temporary files

--- CameraCalibration/TakePicture.py
+++
@@ -1,5 +0,0 @@
-from picamera import PiCamera
-
-camera = PiCamera()
-camera.capture("/home/davidcalles/Pictures/pic0.jpg")
-print("Done.")
--- UserInput/TestOutputToFile.py
+++
@@ -1,7 +0,0 @@
-i=0
-import time
-
-for x in range(20):
-    print(f"Hello {i}\n")
-    i+=1
-    time.sleep(1)
